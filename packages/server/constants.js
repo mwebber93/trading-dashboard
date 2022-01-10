ba6ce77513a1750aa4545352fb306673aa106287
Matt Webber <matthew.webber@outlook.com>
2022-01-10 22:00:54 +0000

    Create Order endpoint updated to also broadcast a message when an order is created successfully.

--- packages/server/constants.js
+++ packages/server/constants.js
@@ -1,5 +1,9 @@
 const PRICE_UPDATE_MESSAGE = 'latest_price_update';
+const NEW_ORDER_MESSAGE = 'new_order';
+const ORDER_EXECUTED_MESSAGE = 'order_executed';
 
 module.exports = {
 	PRICE_UPDATE_MESSAGE,
+	NEW_ORDER_MESSAGE,
+	ORDER_EXECUTED_MESSAGE,
 };
--- packages/server/tradeService/controllers/createOrderController.js
+++ packages/server/tradeService/controllers/createOrderController.js
@@ -1,8 +1,11 @@
 const moment = require('moment');
+const uuid = require('uuid');
 const Validator = require('jsonschema').Validator;
 const validator = new Validator();
 
+const { broadcastMessage } = require('../../webSocketServer');
 const { roundTo2DP } = require('../../helpers');
+const { NEW_ORDER_MESSAGE } = require('../../constants');
 
 const orderSchema = {
 	id: '/order',
@@ -22,15 +25,26 @@ const createNewOrderController = (req, res, data) => {
 			res.status(400).jsonp({ message: errors[0].message });
 		} else {
 			const { type, quantity, price } = req.body;
-
-			data.traderInfo.orders.push({
+			const newOrder = {
+				id: uuid.v4(),
 				type,
 				quantity,
-				price,
+				price: roundTo2DP(price),
 				triggered: false,
 				succeeded: false,
 				timestamp: moment().set({ milliseconds: 0, seconds: 0 }).valueOf(),
-			});
+			};
+
+			data.traderInfo.orders.push(newOrder);
+
+			const broadcastData = {
+				id: newOrder.id,
+				type: newOrder.type,
+				quantity: newOrder.quantity,
+				price: newOrder.price,
+			};
+			// Broadcast new order.
+			broadcastMessage(NEW_ORDER_MESSAGE, broadcastData);
 
 			return res.status(200).jsonp({ message: 'OK' });
 		}
