b9c6f4219721572f044e08f5f17b98eadfffdf95
Matt Webber <matthew.webber@outlook.com>
2022-01-10 15:19:25 +0000

    Added extra validation to the Create Order controller to prevent over buying/selling.

--- packages/server/tradeService/controllers/createOrderController.js
+++ packages/server/tradeService/controllers/createOrderController.js
@@ -2,24 +2,40 @@ const moment = require('moment');
 const Validator = require('jsonschema').Validator;
 const validator = new Validator();
 
+const { roundTo2DP } = require('../../helpers');
+
 const orderSchema = {
 	id: '/order',
 	type: 'object',
 	properties: {
 		type: { type: 'string' },
-		amount: { type: 'number' },
+		quantity: { type: 'number' },
 		price: { type: 'number' },
 	},
-	required: ['type', 'amount', 'price'],
+	required: ['type', 'quantity', 'price'],
 };
 
 const createNewOrderController = (req, res, data) => {
 	try {
 		const { errors } = validator.validate(req.body, orderSchema);
 		if (errors.length > 0) {
-			res.status(400).jsonp(errors[0].message);
+			res.status(400).jsonp({ message: errors[0].message });
 		} else {
 			const { type, quantity, price } = req.body;
+			const { cashAvailable, unitsHeld } = data.traderInfo;
+
+			// Don't allow the trader to place an order they don't have the cash for.
+			const cashRequired = roundTo2DP(quantity * price);
+			if (type === 'buy' && cashRequired > cashAvailable) {
+				res.status(400).jsonp({
+					message: 'You have insufficient funds for this purchase.',
+				});
+			} else if (type === 'sell' && unitsHeld < quantity) {
+				res.status(400).jsonp({
+					message: 'You cannot sell more than you own.',
+				});
+			}
+
 			data.traderInfo.orders.push({
 				type,
 				quantity,
